8df0b464e64e5279b90cd13df98e020d84bf97f3
lynn-twinkl <perez.businessinfo@gmail.com>
2025-03-06 17:38:12 -0500

    var_map

---
+++ var_map.py
@@ -0,0 +1,18 @@
+var_map = {
+        'tpt' : 'Teachers Pay Teachers',
+        'tptcom' : 'Teachers Pay Teachers',
+        'teacherpayteacher' : 'Teachers Pay Teachers',
+        'news to you' : 'n2y',
+        'esl library' : 'Ellii',
+        'good and beautiful' : 'The Good And The Beautiful',
+        'esllibrary' : 'Ellii',
+        'epiccom' : 'Epic',
+        'getepiccom' : 'Epic',
+        'imlovinlit' : "I'm Lovin Lit",
+        'educationcom' : 'Education.com',
+        'superworksheetcom' : 'Super Teacher Worksheets',
+        'carsondellosa' : 'Carson Dellosa Education',
+        'mia academy' : 'Miacademy',
+        'tool 2 grow' : 'Tools To Grow',
+
+        }
